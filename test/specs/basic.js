9c9e513bd98d16682f32a3bfbec59a87c8323761
AbinayaArul <abinaya.palani@a-cti.com>
2020-06-30 14:07:01 +0530

    checking in getTitle command in jenkins

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,9 @@
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
-        browser.url('https://webdriver.io')
+        browser.url('https://staging.access.answerconnect.com')
         const browserTitle = browser.getTitle()
         console.log('Browser title '+browserTitle)
-        browser.click('li.navSearchWrapper.reactNavSearchWrapper');
+        //browser.click('li.navSearchWrapper.reactNavSearchWrapper');
 
         //expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
     })
